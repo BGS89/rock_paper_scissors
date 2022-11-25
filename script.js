68359dad49f0d7d386c7e6692beb409fc9e26580
Ben Sivyer <bensivyer@bens-mbp.home>
2022-11-25 10:35:38 +0000

    final version

--- script.js
+++ script.js
@@ -1,132 +1,101 @@
-const rock = document.getElementById('rock');
-const paper = document.getElementById('paper');
-const scissors = document.getElementById('scissors');
+// BUTTONS
+const selectorButton = document.querySelectorAll('.option');
+const play = document.getElementById('play');
 const reset = document.getElementById('reset');
 
-const playerRock = document.getElementById('player-rock');
-const playerPaper = document.getElementById('player-paper');
-const playerScissors = document.getElementById('player-scissors');
+// ICONS
+const hands = document.querySelectorAll('.play');
+const compHands = document.querySelectorAll('.cplay');
 
-const computerRock = document.getElementById('computer-rock');
-const computerPaper = document.getElementById('computer-paper');
-const computerScissors = document.getElementById('computer-scissors');
-
+// SCORES
 const scoreOne = document.getElementById('score-one');
 const scoreTwo = document.getElementById('score-two');
-
-const response = document.getElementById('response');
-
-const play = document.getElementById('play');
-
-let pSelector = '';
-let compSelector = '';
+const result = document.getElementById('result');
 
 let playerScore = 0;
 let computerScore = 0;
 
-rock.addEventListener('click', function () {
-    pSelector = 'Rock';
-    playerRock.style.display = 'block';
-    playerPaper.style.display = 'none';
-    playerScissors.style.display = 'none';
+let playerOne = '';
 
-    computerRock.style.display = 'none';
-    computerPaper.style.display = 'none';
-    computerScissors.style.display = 'none';
-
-
-});
-
-paper.addEventListener('click', function () {
-    pSelector = 'Paper';
-    playerPaper.style.display = 'block';
-    playerRock.style.display = 'none';
-    playerScissors.style.display = 'none';
-
-    computerRock.style.display = 'none';
-    computerPaper.style.display = 'none';
-    computerScissors.style.display = 'none';
-});
-
-scissors.addEventListener('click', function () {
-    pSelector = 'Scissors';
-    playerScissors.style.display = 'block';
-    playerRock.style.display = 'none';
-    playerPaper.style.display = 'none';
-
-    computerRock.style.display = 'none';
-    computerPaper.style.display = 'none';
-    computerScissors.style.display = 'none';
-});
-
-
-function computerSelector() {
-
-
-
-    let randomNumber = Math.floor((Math.random() * 3) + 1);
-    if (pSelector.length < 1) {
-        alert('Please select Rock, Paper or Scissors');
-    } else if (randomNumber == 1) {
-        compSelector = 'Rock';
-        computerRock.style.display = 'block'
-        computerPaper.style.display = 'none'
-        computerScissors.style.display = 'none'
-
-    } else if (randomNumber == 2) {
-        compSelector = 'Paper';
-        computerPaper.style.display = 'block'
-        computerRock.style.display = 'none'
-        computerScissors.style.display = 'none'
-
-    } else if (randomNumber == 3) {
-        compSelector = 'Scissors';
-        computerScissors.style.display = 'block'
-        computerRock.style.display = 'none'
-        computerPaper.style.display = 'none'
-    }
-    // secondResponse.innerHTML = compSelector;
-
-    if (pSelector === compSelector) {
-        result.innerHTML = 'Draw';
-    } else if (pSelector === 'Rock' && compSelector === 'Paper' ||
-        pSelector === 'Paper' && compSelector === 'Scissors' ||
-        pSelector === 'Scissors' && compSelector === 'Rock') {
-        result.innerHTML = 'You Lose!';
-        computerScore += 1;
-        scoreOne.innerHTML = playerScore;
-        scoreTwo.innerHTML = computerScore;
-    } else {
-        result.innerHTML = 'You Win!'
-        playerScore += 1;
-        scoreOne.innerHTML = playerScore;
-        scoreTwo.innerHTML = computerScore;
-    }
-
+// CLEAR ICONS
+const clearPlayer = () => {
+    hands[0].style.display = 'none';
+    hands[1].style.display = 'none';
+    hands[2].style.display = 'none';
 }
 
+const clearComp = () => {
+    compHands[0].style.display = 'none';
+    compHands[1].style.display = 'none';
+    compHands[2].style.display = 'none';
+}
 
-play.addEventListener('click', computerSelector);
+// PLAYER HAND SELECTION
+selectorButton.forEach(button => {
+    button.addEventListener('click', function () {
+        playerOne = button.id;
+        clearPlayer();
+        switch (button.id) {
+            case 'rock':
+                hands[0].style.display = 'block';
+                break;
+            case 'paper':
+                hands[1].style.display = 'block';
+                break;
+            case 'scissors':
+                hands[2].style.display = 'block';
+                break;
+        }
+    })
+})
 
+// COMPUTER HAND SELCTION
+play.addEventListener('click', function () {
+    clearComp()
+    // Generate random number
+    let randomNumber = Math.floor((Math.random() * 3) + 1);
+    // Apply random number to rock paper or scissors
+    switch (randomNumber) {
+        case 1:
+            computer = 'rock';
+            compHands[0].style.display = 'block'
+            break;
+        case 2:
+            computer = 'paper';
+            compHands[1].style.display = 'block';
+            break;
+        case 3:
+            computer = 'scissors';
+            compHands[2].style.display = 'block';
+            break; 
+    }
 
+    // Compare to player choice 
+    if (playerOne.length < 1) {
+        alert('Please select Rock, Paper or Scissors');
+        clearComp();
+    } else if (playerOne === computer) {
+        result.innerHTML = 'Draw';
+    } else if (playerOne === 'rock' && computer === 'paper' ||
+        playerOne === 'paper' && computer === 'scissors' ||
+        playerOne === 'scissors' && computer === 'rock') {
+        result.innerHTML = 'You Lose!';
+        computerScore += 1;
+        scoreTwo.innerHTML = computerScore;
+    } else {
+        result.innerHTML = 'You Win!';
+        playerScore += 1;
+        scoreOne.innerHTML = playerScore;
+    }
+});
 
+// RESET GAME
 reset.addEventListener('click', function () {
-
+    clearComp();
+    clearPlayer();
     scoreOne.innerHTML = 0;
     scoreTwo.innerHTML = 0;
     playerScore = 0;
     computerScore = 0;
-    pSelector = '';
-    compSelector = '';
-
-    result.innerHTML = '?';
-
-    playerRock.style.display = 'none';
-    playerPaper.style.display = 'none';
-    playerScissors.style.display = 'none';
-    computerRock.style.display = 'none';
-    computerPaper.style.display = 'none';
-    computerScissors.style.display = 'none';
-
-});
+})
 
